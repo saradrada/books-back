153fc8ccbfa6f0af21df2a512fbe44e9756147b3
Alejandro Perdomo <alejandro.perdomob@perficient.com>
2021-02-09 20:37:11 -0500

    feat: add books endpoint

---
+++ src/main/java/automation/practice/controllers/BooksController.java
@@ -0,0 +1,14 @@
+package automation.practice.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+@RequestMapping("books")
+public class BooksController {
+  @GetMapping("")
+  public String index() {
+    return "Greetings from Spring Boot!";
+  }
+}
