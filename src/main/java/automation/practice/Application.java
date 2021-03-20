5e4630fd68604b21a46022712716581160336303
saradrada <sara.ortiz@correo.icesi.edu.co>
2021-03-20 18:42:33 -0500

    heroku deply configuration

--- src/main/java/automation/practice/Application.java
+++ src/main/java/automation/practice/Application.java
@@ -1,14 +1,20 @@
 package automation.practice;
 
 import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Application {
+  @Value("${ALLOWED_ORIGINS:http://localhost:4200}")
+  private String allowedOrigins;
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
@@ -23,6 +29,19 @@ public class Application {
       Arrays.sort(beanNames);
       for (String beanName: beanNames) {
         System.out.println(beanName);
+      }
+    };
+  }
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry
+          .addMapping("/**")
+          .allowedOrigins(allowedOrigins)
+          .allowedMethods("*");
       }
     };
   }
